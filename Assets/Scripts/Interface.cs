8718683a6beed1320f03d4cf8a0cc63283d5a2f5
Conor Brien <conorbrien@gmail.com>
2014-06-09 14:06:42 +0100

    Interface Updates

--- Assets/Scripts/Interface.cs
+++ Assets/Scripts/Interface.cs
@@ -4,13 +4,25 @@ using System.Collections;
 public class Interface : MonoBehaviour
 {
 	public Texture moneyButton;
+	public bool mouseOver =false;
 
 	void OnGUI()
 	{
 
-		float buttonX = (Screen.width - moneyButton.width /2.0f);
-		float buttonY = (Screen.height - moneyButton.height /2.0f);
+		float buttonX = (Screen.width - (moneyButton.width /2.2f));
+		float buttonY = (Screen.height -(moneyButton.height /2.2f));
 
-		GUI.Button( new Rect(buttonX, buttonY, moneyButton.width /2, moneyButton.height /2.0f),moneyButton ); 
+		GUI.DrawTexture( new Rect(buttonX, buttonY, moneyButton.width /2.2f, moneyButton.height /2.2f),moneyButton, ScaleMode.ScaleToFit, true); 
+	}
+	void onMouseOver()
+	{
+		mouseOver = true;
+		Debug.Log ("Entered");
+	}
+
+	void onMouseExit()
+	{
+		mouseOver = false;
+		Debug.Log ("Exited");
 	}
 }
