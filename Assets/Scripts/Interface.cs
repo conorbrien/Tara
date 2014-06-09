84c9422204e983b7bd5993ee42f3744b2f51d7a7
Conor Brien <conorbrien@gmail.com>
2014-06-09 11:47:39 +0100

    Completed MoneyCard and started GUI

---
+++ Assets/Scripts/Interface.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interface : MonoBehaviour
+{
+	public Texture moneyButton;
+
+	void OnGUI()
+	{
+
+		float buttonX = (Screen.width - moneyButton.width /2.0f);
+		float buttonY = (Screen.height - moneyButton.height /2.0f);
+
+		GUI.Button( new Rect(buttonX, buttonY, moneyButton.width /2, moneyButton.height /2.0f),moneyButton ); 
+	}
+}
