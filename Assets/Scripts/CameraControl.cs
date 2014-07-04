a2a97d7a659198239b6b4501f0cb4ad695f2e7f8
Conor Brien <conorbrien@gmail.com>
2014-07-04 10:59:35 +0100

    Camera Fundamentals

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour 
+{
+	//variables to hold the X,Y,Z value of the camera
+	public float posX;
+	public float posY;
+	public float posZ;
+	public float cameraVelocity;
+	public float zoomVelocity;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Detect initial position of camera
+		posX = transform.position.x;
+		posY = transform.position.y;
+		posZ = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Left
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			posX += cameraVelocity*Time.deltaTime;
+			posZ -= cameraVelocity*Time.deltaTime;
+			transform.position = new Vector3(posX,posY,posZ);
+		}
+		//Right
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			posX -= cameraVelocity*Time.deltaTime;
+			posZ += cameraVelocity*Time.deltaTime;
+			transform.position = new Vector3(posX,posY,posZ);
+		}	
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			posZ += cameraVelocity*Time.deltaTime;
+			transform.position = new Vector3(posX,posY,posZ);
+		}	
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			posZ -= cameraVelocity*Time.deltaTime;
+			transform.position = new Vector3(posX,posY,posZ);
+		}	
+
+
+		if (Input.GetAxis("Mouse ScrollWheel") < 0) 
+		{
+			posY += zoomVelocity*Time.deltaTime;
+			transform.position = new Vector3(posX,posY,posZ);
+		}
+		//Down
+		if (Input.GetAxis("Mouse ScrollWheel") > 0) 
+		{
+			posY -= zoomVelocity*Time.deltaTime;
+			transform.position = new Vector3(posX,posY,posZ);
+		}
+	}
+}
